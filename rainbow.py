5dc8e18c601e9dff233c9fe1a808b7db5ed3a978
Socialery <>
2013-08-19 02:08:59 +0930

    Updating pythonicness

--- rainbow.py
+++ rainbow.py
@@ -5,6 +5,7 @@ __module_description__ = "Rainbow command"
 
 #Globals
 r=0
+colorf='\003%02d'
 
 #Callbacks
 def rainbow_cb(word, eol, userdata):
@@ -12,12 +13,10 @@ def rainbow_cb(word, eol, userdata):
 		print("Rainbow: Need a message")
 	else:
 		global r
-		in=eol[1]
-		# could use a map for the following? yes, yes I could, noted.
-		color=lambda a,b:'\003%02d'%((a+b)%14+2) #\003 folowed by 02-15 I think?
-		out=[color(r,n)+in[n] for n in range(len(in))] #then append that to each char
-		out=''.join(out) #and concat
-		r=(r+1)%14 #then rotate the initial color
+		inp = enumerate(eol[1],r)
+		f = lambda x: (colorf%((x[0])%14+2))+str(x[1])
+		out = ''.join(map(f,inp))
+		r = (r+1)%14
 		xchat.command("SAY "+out)
 	return xchat.EAT_XCHAT
 
